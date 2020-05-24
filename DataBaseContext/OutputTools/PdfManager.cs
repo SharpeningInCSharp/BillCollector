c868d765275c9a2ed74a37ac93d5302a921ca170
Fedor Ishchenko <aleks180700@gmail.com>
2020-05-24 17:37:56 +1000

    Pdf output view addited.
    Need to do:
    1. reduce font size in date
    2. underline "TOTAL" only
    3. add space after "TOTAL"
    4. add 2-3 chars to Separator
    5. "bla-bla..." to normal text

--- DataBaseContext/OutputTools/PdfManager.cs
+++ DataBaseContext/OutputTools/PdfManager.cs
@@ -9,9 +9,15 @@ namespace DataBaseContext.OutputTools
 	public static class PdfManager
 	{
 		private const string Header = "Receip";
-		private const string PostHeaderInfo = "Bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla,\nBla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla";
+		private const string PreDate = "Purchase time:";
+		private const string PostHeaderInfo = "Bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla";
+		private const int HeaderFontSize = 24;
 		private const int MainFontSize = 14;
-		private readonly static string Separator = new string('*', 20);
+		private const int HeaderTopMargin = 20;
+		private const int TotalTextFontSize = 20;
+		private const int TotalSumFontSize = 18;
+		private readonly static string Separator = new string('*', 33);
+
 		//private readonly static string DirPath = @"C:\Users\User\source\repos\BillCollector\DataBaseContext\Bills\";
 		private readonly static string DirPath = @"C:\Users\aleks\Source\Repos\BillCollector\DataBaseContext\Bills\";
 
@@ -30,33 +36,32 @@ namespace DataBaseContext.OutputTools
 		{
 			var filePath = System.IO.Path.Combine(DirPath, expence.IdentityGuid.ToString() + ".pdf");
 
-			var pdfWriter = new PdfWriter(filePath);
-			var pdfDoc = new PdfDocument(pdfWriter);
-			var doc = new Document(pdfDoc);
-			///SOLVE: apply B7 - size, add Margin before Header. Find out problem with crossed sum. Out Date in format: dd.mm.yy. hh.mm
+			var pdfDoc = new PdfDocument(new PdfWriter(filePath));
+			var doc = new Document(pdfDoc, PageSize.B7);
+			///SOLVE: apply B7 - size, add Margin before Header. Find out problem with crossed sum. Out Date in format: hh.mm dd.mm.yy.
 			///then figure out Separator length OR find way to fill hole line with it
-			var docArea = doc.GetPageEffectiveArea(PageSize.B7);
-			doc.Add(new AreaBreak());
-			var header = new Paragraph(Header).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER).SetFontSize(24);
-			doc.Add(header).SetFontSize(MainFontSize);
+			doc.Add(new Paragraph(Header).SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+										.SetFontSize(HeaderFontSize)
+										.SetMarginTop(HeaderTopMargin));
 			doc.Add(new Paragraph(PostHeaderInfo));
-			doc.Add(new Paragraph(Separator));
 
-			decimal totalSum = 0;
+			doc.Add(new Paragraph(Separator));
 			foreach (var item in expence.Goods)
 			{
-				var curTotSum = item.Key.Price * item.Value;
-				totalSum += curTotSum;
-				var resStr = $"{item.Value} {item.Key.Name} - {curTotSum}";
-				var par = new Paragraph(resStr).SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT).SetFontSize(MainFontSize);
-				doc.Add(par);
+				var itemStr = $"{item.Value} {item.Key.Name} - {item.Key.Price * item.Value}";
+				doc.Add(new Paragraph(itemStr).SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT)
+											.SetFontSize(MainFontSize));
 			}
-			//TODO: total sum with 2 sighns before . and with P;
 			doc.Add(new Paragraph(Separator));
-			//TODO:add paragraph preSum - TOTAL:
-			doc.Add(new Paragraph(totalSum.ToString()).SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT).SetFontSize(20).SetLineThrough());
-			//TODO:add paragraph preDate - Purchase time
-			doc.Add(new Paragraph(expence.Date.ToShortDateString()).SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT).SetFontSize(MainFontSize));
+			
+			var sumParagraph = new Paragraph().SetTextAlignment(iText.Layout.Properties.TextAlignment.LEFT);
+			sumParagraph.Add("TOTAL:").SetFontSize(TotalTextFontSize).SetUnderline();
+			sumParagraph.Add($"{expence.Sum:C2}").SetFontSize(TotalSumFontSize);
+			doc.Add(sumParagraph);
+
+			doc.Add(new Paragraph($"{PreDate} {expence.Date:hh:mm dd.mm.yyyy}")
+					.SetTextAlignment(iText.Layout.Properties.TextAlignment.RIGHT)
+					.SetFontSize(MainFontSize));
 
 			doc.Close();
 			return filePath;
