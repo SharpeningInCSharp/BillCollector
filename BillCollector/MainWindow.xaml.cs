b8c4aac3ce3ec3ce2b8b5bdf63a933e08829a786
Fedor Ishchenko <aleks180700@gmail.com>
2020-05-25 23:41:15 +1000

    Page separation was added
    Now BillCollector's MainWindow contains
    Frame which display required page:
    BillsPage of StatisticPage
    
    BillsPage must contain all stuff to manage user's bills
    
    StatisticPage contains PieDiagram and other stuff

--- BillCollector/MainWindow.xaml.cs
+++ BillCollector/MainWindow.xaml.cs
@@ -18,13 +18,6 @@ namespace BillCollector
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public DateTime StartDareTime { get; } = DateTime.Today;
-		private readonly DateTime InitialDate = new DateTime(2019, 1, 1);
-		public PieDiagram diagram;
-
-		private Func<GoodType, DateTime, DateTime?, IEnumerable<Expence.ExpenceSelection>> dataProvider;
-		private readonly SolidColorBrush[] UserBrushes = new SolidColorBrush[] { Brushes.Red, Brushes.Blue, Brushes.Green, Brushes.Purple, Brushes.Cyan, Brushes.Orange };
-
 		//SOLVE: update button
 		//SOLVE: add list of bills
 		//SOLVE: add button to load bill
@@ -32,66 +25,20 @@ namespace BillCollector
 		{
 			InitializeComponent();
 
-			Display();
+			OpenStatisticManagerButton_Click(OpenStatisticManagerButton, null);
+			//out statistic first
 		}
 
-		private async void Display()
+		private void OpenBillsManagerButton_Click(object sender, RoutedEventArgs e)
 		{
-			dataProvider = DataBase.SelectAndDistinct;
-
-			Calendar.DisplayDateStart = new DateTime(InitialDate.Year, InitialDate.Month, InitialDate.Day);
-
-			Initialize();
-
-			await Task.Run(() => CrossOutEmptyDates());
+			var billsUri = new Uri(@"BillsPage.xaml", UriKind.Relative);
+			PageFrame.Source = billsUri;
 		}
 
-		private void CrossOutEmptyDates()
+		private void OpenStatisticManagerButton_Click(object sender, RoutedEventArgs e)
 		{
-			var avaialbleDates = DataBase.GetAvailableDates();
-			var currentDate = DateTime.Today;
-			while (currentDate != InitialDate)
-			{
-				if (avaialbleDates.Count(x => x.Year == currentDate.Year &&
-										x.Month == currentDate.Month &&
-										x.Day == currentDate.Day) == 0)     // if there's no such date
-				{
-					Dispatcher.Invoke(() => Calendar.BlackoutDates.Add(new CalendarDateRange(currentDate)));
-				}
-
-				currentDate = currentDate.AddDays(-1);
-			}
-		}
-
-		private void Initialize()
-		{
-			DateTime lastExpenceD;
-			Calendar.SelectedDate = lastExpenceD = DataBase.GetLastExpenceDate();
-			Calendar.DisplayDateEnd = DateTime.Today;
-
-			var sc = new Scopes<GoodType, Expence.ExpenceSelection>(dataProvider, typeof(GoodType), lastExpenceD, null);
-			diagram = new PieDiagram(sc, UserBrushes);
-			Grid.SetRow(diagram, 0);
-			Grid.SetColumn(diagram, 1);
-			mainGrid.Children.Add(diagram);
-		}
-
-		private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
-		{
-			if (diagram != null)
-			{
-				Mouse.Capture(null);
-
-				var dates = Calendar.SelectedDates.OrderBy(x => x.Date);
-				DateTime initial = dates.First();
-				DateTime? final = dates.Last();
-
-				if (initial == final.Value)
-					final = null;
-
-				var sc = new Scopes<GoodType, Expence.ExpenceSelection>(dataProvider, typeof(GoodType), initial, final);
-				diagram.LoadNew(sc);
-			}
+			var statUri = new Uri(@"Pages\StatisticPage.xaml", UriKind.Relative);
+			PageFrame.Source = statUri;
 		}
 	}
 }
